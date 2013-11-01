b417f9be6c1b575bfe8c30bc8c20cfb412e540e6
evgeniy <evgeniy@evgeniy-eMachines-E725.(none)>
2013-11-02 00:30:47 +0200

    Added win1251 version

--- conv.php
+++ conv.php
@@ -1,6 +1,7 @@
 <?php
-$a = file_get_contents('index.html');
-$string = iconv('cp1251', 'utf-8', $a);
-var_dump(file_exists('index_utf8.html'), is_writable('index_utf8.html'));
-var_dump(file_put_contents('index_utf8.html', $string));
+$a = file_get_contents('index_utf8.html');
+//$string = iconv('cp1251', 'utf-8', $a);
+$string = iconv('utf-8', 'cp1251', $a);
+
+var_dump(file_put_contents('index.html', $string));
 ?>
